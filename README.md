860232d67eefc07c76ac49c883911df47d5332b6
youjia <youjia@e.com>
2022-09-24 21:26:54 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,119 @@
+browser-touchlib
+===
+
+| **browser-touchlib**
+| [browser-touchlib](https://github.com/x2jia/touch-lib)
+
+![](./gif/horizon.gif)
+
+```html
+<!DOCTYPE html>
+<html>
+  <head>
+  <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport"/>
+  <meta charset='utf-8'>
+  <script type="text/javascript" charset="utf-8" src="http://cdn.staticfile.org/jquery/1.12.4/jquery.min.js"></script>
+  <script type="text/javascript" charset="utf-8" src="./../index.js"></script>
+  <title>test</title>
+  <style>
+    html, body {
+      background-color: #fff;
+      background-repeat: no-repeat;
+      -webkit-font-smoothing: antialiased;
+      height: 100%;
+      font-size: 14px;
+    }
+    .area {
+      background-color: #ccc;
+      width: 300px;
+      height: 300px;
+      margin: auto;
+    }
+  </style>
+  <script type="text/javascript">
+    $(function(){
+      const id1 = window.jFingerSlideMob.register('area', {
+        handler(event){
+          console.log(event.type, event)
+        },
+        option: {
+          scale: 1,
+          orientation: '-|',
+          style: 'font-size: 54px',
+          tip: true
+        }
+      })
+      setTimeout(() => {
+        window.jFingerSlideMob.unregister(id1)
+      }, 5000)
+
+    })
+  </script>
+</head><body>
+  <div id="area" class="area" style="touch-action: none;" >
+    hello
+  </div>
+</body></html>
+```
+
+![](./gif/vertical.gif)
+
+```js
+    const jFingerSlideMob = require('browser-touchlib')($)
+
+    const moveid = jFingerSlideMob.register('midea-wrapper', {
+      handler: (event) => {
+        if(event.type !== 'touchmove'){
+          this.state.currentTime = -1;
+        }else{
+          const duration = myPlayer.duration()
+          if(typeof duration === 'number' && duration > 0){
+            if(this.state.currentTime < 0){
+              this.state.currentTime = myPlayer.currentTime()
+            }
+
+            var progress = Math.floor(this.state.currentTime + event.progressX * duration)
+            if(progress > duration){
+              progress = duration
+            }
+            if(progress < 0){
+              progress = 0
+            }
+            myPlayer.currentTime(progress)
+          }
+        }
+      },
+      option: {
+        scale: 0.7,
+        orientation: '-',
+        style: 'font-size: 54px; color: #fe9; position: absolute; top: 0; right: 0;',
+        tip: true,
+      }
+    })
+```
+
+### API
+
+**register(id, object)**
+
+***id***, string,  An html element id or null
+
+***object*** : {
+  handler, A callback function,  (event) => {}
+  option: {
+    scale，number,  (0, ∞)
+    style，string
+    tip, boolean
+    orientation, string,  {"-","|","-|"}
+  }
+}
+***return value***
+A unique value，unregister will use it
+
+**unregister(return_value)**
+
+***return_value***, A unique value returned by register
+
+### License
+
+996ICU License
---
+++ index.js
@@ -0,0 +1,295 @@
+var _$, _index, _obj, _visualtipId, _defops, _orientation
+
+if(typeof $ !== 'undefined'){
+  _$ = $
+}
+
+_index = 0
+_obj = {}
+_visualtipId = `jfsm-${(new Date()).getTime()}`
+_defops = {
+  scale: 1,
+  /*
+    -, |, -|, |-
+    1, 2,  3, 4
+  */
+  orientation: 3,
+  style: '',
+  scrolling: false,
+  tip: false,
+  tolerance: 'default', /* infinite, default */
+}
+_orientation = ['^', '-', '|']
+
+function newkey(){
+  return `${++_index}${(new Date()).getTime()}`
+}
+
+function wrapfn(fn, k, type){
+  return function(event){
+    innerproc(k, type, event, fn)
+  }
+}
+
+function appendhml(jParent, kvalue){
+  if(!kvalue.option.tip){
+    return
+  }
+  const html = `<p id="${_visualtipId}"
+    style="user-select: none; font-size: 64px; color: #0f0; width: 100%; text-align: center; ${kvalue.option.style}">0</p>`
+  jParent.append(html)
+}
+
+function updatehml(kvalue){
+  if(!kvalue.option.tip){
+    return
+  }
+  if(kvalue.context.orientation < 3){
+    const currentpoint = kvalue.context.pointArr[kvalue.context.pointArr.length - 1]
+    if(kvalue.context.orientation < 2){
+      var progress = currentpoint.progressX
+    }else{
+      progress = currentpoint.progressY
+    }
+    _$(`#${_visualtipId}`).text(`${progress < 0 ? '-' : '+'}${(Math.abs(progress) * 100).toFixed(1)}%`)
+  }
+}
+
+function clearpanel(){
+  _$(`#${_visualtipId}`).remove()
+}
+
+function eventproc(event, options){
+  if(!options.scrolling){
+    event.preventDefault()
+  }
+}
+
+function innerproc(k, type, event, fn){
+  const it = _obj[k]
+  if(!it){
+    return
+  }
+
+  if(event.changedTouches.length < 1){
+    return
+  }
+
+  if(it.id){
+    var ele = _$(`#${it.id}`)
+  }else{
+    ele = _$(document.body)
+  }
+
+  const rawtouch = event.changedTouches[event.changedTouches.length - 1]
+  const currentpoint = {
+    clientX: rawtouch.clientX,
+    clientY: rawtouch.clientY,
+    pageX: rawtouch.pageX,
+    pageY: rawtouch.pageY,
+    radiusX: rawtouch.radiusX,
+    radiusY: rawtouch.radiusY,
+    screenX: rawtouch.screenX,
+    screenY: rawtouch.screenY,
+    type,
+    progressX: 0,
+    progressY: 0,
+    deltaX: 0,
+    deltaY: 0,
+    orientation: it.context.orientation < 3 ? _orientation[it.context.orientation] : '-|'
+  }
+  const pointArr = it.context.pointArr
+
+  if(type === 'touchstart'){
+    if(it.context.status === 'idle'){
+      it.context.status = 'busy'
+      it.context.width  = ele.width()  > 0 ? ele.width()  : 1
+      it.context.height = ele.height() > 0 ? ele.height() : 1
+      pointArr.push(Object.assign({}, currentpoint))
+      appendhml(ele, it)
+      fn(currentpoint)
+    }else{
+      pointArr[pointArr.length - 1].type = 'touchend'
+      fn(pointArr[pointArr.length - 1])
+      it.context.status = 'idle'
+      it.context.pointArr = []
+      it.context.orientation = it.option.orientation
+      clearpanel()
+    }
+  }else if(type === 'touchmove'){
+    if(it.context.status === 'busy'){
+      if(pointArr.length > 2){
+        Object.assign(pointArr[pointArr.length - 2], pointArr[pointArr.length - 1])
+        pointArr.pop()
+      }
+      const bginpnt = pointArr[0]
+      const deltaX = currentpoint.pageX - bginpnt.pageX
+      const deltaY = bginpnt.pageY - currentpoint.pageY
+      if(it.context.orientation > 2){
+        it.context.orientation = Math.abs(deltaY) > Math.abs(deltaX) ? 2 : 1
+        currentpoint.orientation = _orientation[it.context.orientation]
+      }
+      currentpoint.progressX = (deltaX / it.context.width)  * it.option.scale
+      currentpoint.progressY = (deltaY / it.context.height) * it.option.scale
+      currentpoint.deltaX = ((currentpoint.pageX - pointArr[pointArr.length - 1].pageX) / it.context.width)  * it.option.scale
+      currentpoint.deltaY = ((pointArr[pointArr.length - 1].pageY - currentpoint.pageY) / it.context.height) * it.option.scale
+      pointArr.push(Object.assign({}, currentpoint))
+      updatehml(it)
+      fn(currentpoint)
+    }
+  }else if(type === 'touchend' || type === 'touchcancel'){
+    if(it.context.status === 'busy'){
+      pointArr[pointArr.length - 1].type = type
+      fn(pointArr[pointArr.length - 1])
+      it.context.status = 'idle'
+      it.context.pointArr = []
+      it.context.orientation = it.option.orientation
+      clearpanel()
+    }
+  }
+  eventproc(event, it.option)
+
+}
+
+function register(...values){
+  const k = newkey()
+  var touchstart, touchmove, touchend, touchcancel, id
+  var touchfn_, option_
+  if(values.length === 1){
+    if(typeof values[0] === 'object'){
+      touchfn_ = values[0].handler
+      option_ = values[0].option
+    }
+  }else if(values.length === 2){
+    if(typeof values[0] === 'string'){
+      id = values[0]
+    }
+    if(typeof values[1] === 'object'){
+      touchfn_ = values[1].handler
+      option_ = values[1].option
+    }
+  }else{
+    return null
+  }
+
+  if(id){
+    var ele = document.getElementById(id)
+  }else{
+    ele = document.body
+  }
+
+  if(!(ele && ele.addEventListener) || !(typeof touchfn_ === 'function')){
+    return null
+  }
+
+  touchstart  = wrapfn(touchfn_, k, 'touchstart')
+  touchmove   = wrapfn(touchfn_, k, 'touchmove')
+  touchend    = wrapfn(touchfn_, k, 'touchend')
+  touchcancel = wrapfn(touchfn_, k, 'touchcancel')
+
+  ele.addEventListener('touchstart', touchstart, { passive: false })
+  ele.addEventListener('touchmove', touchmove, { passive: false })
+  ele.addEventListener('touchend', touchend, { passive: false })
+  ele.addEventListener('touchcancel', touchcancel, { passive: false })
+
+  if(typeof option_ === 'object'){
+    if(typeof option_.scale === 'number'){
+      if(!(option_.scale > 0)){
+        option_.scale = _defops.scale
+      }
+    }else{
+      option_.scale = _defops.scale
+    }
+
+    if(typeof option_.orientation === 'string'){
+      const h_ = option_.orientation.indexOf('-')
+      const v_ = option_.orientation.indexOf('|')
+      let orientation = 0
+      orientation = h_ < 0 ? orientation : (orientation + 1)
+      orientation = v_ < 0 ? orientation : (orientation + 2)
+      if(v_ < h_ && orientation > 2){
+        orientation += 1
+      }
+      if(orientation < 1){
+        orientation = 3
+      }
+      option_.orientation = orientation
+    }else{
+      option_.orientation = 3
+    }
+
+    if(!(option_.tolerance === 'infinite' || option_.tolerance === 'default')){
+      option_.tolerance = 'default'
+    }
+
+    if(!(typeof option_.style === 'string' && option_.style.length > 0)){
+      option_.style = _defops.style
+    }
+
+    option_.scrolling = !!(option_.scrolling)
+
+    option_.tip = !!(option_.tip)
+
+  }else{
+    option_ = _defops
+  }
+
+  _obj[k] = {
+    id: id ? id : null,
+    touchstart,
+    touchmove,
+    touchend,
+    touchcancel,
+    option: option_,
+    context: {
+      status: 'idle',
+      orientation: option_.orientation,
+      pointArr: [],
+      width:  1,
+      height: 1
+    }
+  }
+
+  return k
+
+}
+
+function unregister(k){
+  if(!k){
+    return
+  }
+  const it = _obj[k]
+  if(!it){
+    return
+  }
+
+  if(!it.id){
+    var ele = document.body
+  }else{
+    ele = document.getElementById(it.id)
+  }
+
+  if(ele && ele.removeEventListener){
+    ele.removeEventListener('touchstart', it.touchstart);
+    ele.removeEventListener('touchmove', it.touchmove);
+    ele.removeEventListener('touchend', it.touchend);
+    ele.removeEventListener('touchcancel', it.touchcancel);
+    clearpanel()
+    delete _obj[k]
+  }
+
+}
+
+const exports = {
+  register,
+  unregister
+}
+
+if(typeof module === 'object' && module.exports){
+  module.exports = function(o){
+    _$ = o
+    return exports
+  }
+}else if(typeof window === 'object'){
+  window.jFingerSlideMob = exports
+}
